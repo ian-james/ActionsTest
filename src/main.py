b5c0bf2728d806fb9668871170487923bc86e25b
ian-james <jamey.fraser@gmail.com>
2025-09-12 10:27:35 -0300

    Adding hello function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import os
 import pandas as pd
 
+def helloYou( name ):
+    print(f" Hello {name} ")
 
 def main():
     print("Hello, Welcome to your first Pandas Project")
+
+    helloYou("James")
 
 if __name__ == "__main__":
     main()
